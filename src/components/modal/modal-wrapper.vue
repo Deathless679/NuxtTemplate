c1f06129a8488cac7456a1688d980d6350579dc5
Deathless <switchbladeamv@gmail.com>
2024-07-30 23:05:15 +0500

    update

---
+++ src/components/modal/modal-wrapper.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  position: 'l-top' | 'm-top' | 'r-top' | 'center'
+    | 'l-bottom' | 'm-bottom' | 'r-bottom'
+  title?: string
+  variant: 'default' | 'other'
+}>(), {
+  position: 'center',
+  variant: 'default',
+  title: 'My Modal Title',
+});
+</script>
+
+<template>
+  <div class="modal">
+    <div class="modal__wrapper">
+      <div class="modal__head">
+        <template v-if="!$slots.head">
+          <div class="modal__head-title">
+            {{ title }}
+          </div>
+          <ui-button variants="icon" class="modal__close">
+            Close
+          </ui-button>
+        </template>
+        <template v-else>
+          <slot name="head"></slot>
+        </template>
+      </div>
+      <div class="modal__body">
+        <slot name="default"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
